6a7b116d4683dbbad81e03602daf283feaf8eb8a
shoaib <shoaibahmed2@live.com>
2022-11-18 22:54:40 +0500

    new commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// var numbers=[1,2,3,4,5,6,7]
+
+// console.log("numbers are = ", numbers);
+//  //write a code which which will multiply these numbers with 2 and add them in new array
+ 
+//  var newnumber = []
+//  for (let index = 0; index < numbers.length; index++) {
+//     const element = numbers[index];
+//     newnumber.push(element*2)
+    
+//  }
+// //  console.log("new numbers are = ", newnumber);
+//  const someOFnumber = (a,b) => a+b
+//  console.log(someOFnumber(2,3))
+
+var user1
